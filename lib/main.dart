10c0c54a9873d9511a7857fea14ea79760387ce4
SoonHong Kwon <ksh7512@uangel.com>
2022-10-14 17:35:44 +0900

    [Add] NavigatorObserver

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:navigation/screen/home_screen.dart';
 import 'package:navigation/screen/route_one_screen.dart';
@@ -20,6 +22,33 @@ void main() {
         TWO_ROUTE: (context) => RouteTwoScreen(),
         THREE_ROUTE: (context) => RouteThreeScreen(),
       },
+      navigatorObservers: [MyNavigatorObserver()],
     ),
   );
+}
+
+class MyNavigatorObserver extends NavigatorObserver {
+  @override
+  void didPush(Route route, Route? previousRoute) {
+    log('didPush');
+    super.didPush(route, previousRoute);
+  }
+
+  @override
+  void didPop(Route route, Route? previousRoute) {
+    log('didPop');
+    super.didPop(route, previousRoute);
+  }
+
+  @override
+  void didRemove(Route route, Route? previousRoute) {
+    log('didRemove');
+    super.didRemove(route, previousRoute);
+  }
+
+  @override
+  void didReplace({ Route<dynamic>? newRoute, Route<dynamic>? oldRoute }) {
+    log('didReplace');
+    super.didReplace();
+  }
 }
